9aea7b9ca6d9ed218df5dc49b1760a5a19962134
flyvbee <appoliverapp@gmail.com>
2016-05-02 21:13:52 -0700

    play stackoverflow

---
+++ playscrapy/stackoverflow/stackoverflow/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class StackoverflowItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    url = scrapy.Field()
+    N_votes = scrapy.Field()
+    N_answers = scrapy.Field()
+    N_view = scrapy.Field()
+    N_view = scrapy.Field()
+    
+    pass
