76add4fc1f7d091720ae0edbc6e4ceec35d77671
flyvbee <appoliverapp@gmail.com>
2016-05-29 22:34:48 -0700

    add the exception handling for no question situation

--- event-driven/spider_question.py
+++ event-driven/spider_question.py
@@ -2,57 +2,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-# url="http://stackoverflow.com/questions/37188212/implementing-logout-in-a-restful-webservice"
-# url="http://stackoverflow.com/questions/37210656/fragment-is-in-backstack"
-
-
 def spider_question(url, verbose=1):
 	# url="http://stackoverflow.com/questions/37210656/fragment-is-in-backstack"
+	# url="http://stackoverflow.com/questions/37448339/cuda-cpu-to-gpu-c-please-advise"
 	# verbose=1
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content, 'html.parser')
-
 	# find the question, each page has only one question
 	question_data1 = soup.find_all("td",{"class":"postcell"})
-	# this is necessary, otherwise, some extra data and words will come.
-	question_data = question_data1[0].contents[1].find_all("div", {"class":"post-text"})
+	# this is necessary, sometimes, "This question link below has been deleted!" quickly.
 	try:
+		question_data = question_data1[0].contents[1].find_all("div", {"class":"post-text"})
 		raw_question = question_data[0].get_text()
 		if verbose:
 			print raw_question
-	except: 
-		pass
-
+	except IndexError:
+		print "\nThis question link below has been deleted!"
+		print url
+		raw_question="Qustion deleted!"
 
 	# find the answers. Each page may have no answer or multiple answers. 
 	answer_data1 = soup.find_all("td",{"class":"answercell"})
-	# for item in answer_data1:
-	# 	try:
-	# 		raw_answers = item.contents[1].get_text()
-	# 	except: 
-	# 		raw_answers= "NO ANSWERs YET!"
-	# 	if verbose:
-	# 		print '------------'
-	# 		print raw_answers
 
 	raw_answers=[]
 	try:
 		for item in answer_data1:
 			raw_answers.append(item.contents[1].get_text())
+			if verbose:
+				print '------------'
+				print raw_answers
 	except:
-		pass
-
-	# except: 
-	# 	raw_answers= "NO ANSWERs YET!"
-
-	if verbose:
-		print '------------'
-		print raw_answers
-
-
-
+		print "This question link below has no answers yet! \n"
+		print url
+		raw_answers="No answers for this question!"
 
 	return [raw_question, raw_answers]
+
+
+
+
+
+
+
+
+
 
 # try:
 # 	pass
@@ -63,3 +56,11 @@ def spider_question(url, verbose=1):
 # finally:
 # 	pass
 	
+	# for item in answer_data1:
+	# 	try:
+	# 		raw_answers = item.contents[1].get_text()
+	# 	except: 
+	# 		raw_answers= "NO ANSWERs YET!"
+	# 	if verbose:
+	# 		print '------------'
+	# 		print raw_answers
